f8a1201908143afa8920833a5fa0de90efbed8a2
Tawanda <tawandamuzanenhamo14@gmail.com>
2020-03-27 21:44:07 +0200

    Inter Slice handover implemented

--- Calculations.py
+++ Calculations.py
@@ -3,35 +3,30 @@ from Slices import Slices, Slice
 from UseCases import UseCases
 
 
-class Calculations:
+class IntraCalculations:
     capacity = 0
     new_thresh = 0
     handoff_thresh = 0
     handoff_rate = 0.5
     holding_time1 = handoff_rate + 0.5
     holding_time2 = handoff_rate + 0.5
+    eMBB_handoff_thresh = 0
+    eMBB_newcall_thresh = 0
+    uRLLC_handoff_thresh = 0
+    uRLLC_newcall_thresh = 0
+    uRLLC_capacity = 0
+    eMBB_capacity = 0
 
     def __init__(self):
         print('Calculations started\n')
 
     @classmethod
     def factorial(cls, num):
+        """Function that calculates the Factorial of a given number"""
         if num == 1 or num == 0:
             return 1
         else:
             return num * cls.factorial(num - 1)
-
-    @classmethod
-    def power(cls, num, power):
-        if power == 0:
-            return 1
-        elif power == 1:
-            return num
-        else:
-            value = 1
-            for i in range(power):
-                value *= num
-            return value
 
     @classmethod
     def probabilities(cls, numerator, denominator):
@@ -64,11 +59,11 @@ class Calculations:
 
         if slice_name == 'eMBB':
             required_bbu = UseCases()
-            bbuN = required_bbu.allocate_bbu(slice_name)[0]
-            bbuH = required_bbu.allocate_bbu(slice_name)[1]
-            cls.capacity = params[0]
-            cls.new_thresh = params[1]
-            cls.handoff_thresh = params[2]
+            bbuN = required_bbu.allocate_bbu(slice_name)[0]  # new call bbU
+            bbuH = required_bbu.allocate_bbu(slice_name)[1]  # handoff call bbu
+            cls.capacity = params[0]  # slice capacity
+            cls.new_thresh = params[1]  # new call threshold
+            cls.handoff_thresh = params[2]  # handoff call threshold
             # Available bandwidth
             newB *= (cls.capacity / (cls.capacity * 3)) * callA
             handB *= (cls.capacity / (cls.capacity * 3)) * handA
@@ -150,7 +145,6 @@ class Calculations:
                                     G1[j] = G1[j] + qn[j] * qh[j]
 
                                 # Condition for dropping handoff calls
-
                                 if bbuN + (
                                         bbuN * newC + bbuH * handC) > cls.capacity or bbuH + bbuH * handC > \
                                         cls.handoff_thresh:
@@ -186,6 +180,187 @@ class Calculations:
                             if bbuN * newC > cls.capacity or bbuN + bbuN * newC > cls.new_thresh:
                                 G3[j] += qn[j]
 
+            # return the probabilities
+            blocking_prob = cls.probabilities(G3, G)
+            return slice_name, blocking_prob
+
+
+class InterCalculations(IntraCalculations):
+
+    def __init__(self):
+        print('Calculations started\n')
+
+    @classmethod
+    def calculations(cls, slice_name, params):
+        # blocking probabilities
+        Dp = np.zeros(10)
+        Bp = np.zeros(10)
+        qn = np.zeros(10)
+        qh = np.zeros(10)
+        # Normalisation Constants
+        G = np.zeros(10)
+        G1 = np.zeros(10)
+        G2 = np.zeros(10)
+        G3 = np.zeros(10)
+        # New Call arrival rates
+        callA = np.arange(1.0, 6, 0.5)  # eMBB, uRLLC, mMTC
+        # Handoff Call Arrival rates
+        handA = (callA * cls.handoff_rate) / 0.5
+        # Initialise Available Bandwidth
+        newB = np.ones(len(callA))
+        handB = np.ones(len(handA))
+        # Initialise Loads
+        newL = np.ones(len(newB))
+        handL = np.ones(len(handB))
+
+        if slice_name == 'eMBB':
+            required_bbu = UseCases()
+            bbuN = required_bbu.allocate_bbu(slice_name)[0]  # new call bbU
+            bbuH = required_bbu.allocate_bbu(slice_name)[1]  # handoff call bbu
+            cls.capacity = params[0]  # slice capacity
+            cls.new_thresh = params[1]  # new call threshold
+            cls.handoff_thresh = params[2]  # handoff call threshold
+            cls.uRLLC_handoff_thresh = params[2] + 10
+            cls.uRLLC_newcall_thresh = params[1] + 5
+            cls.uRLLC_capacity = params[0] + 10
+
+            # Available bandwidth
+            newB *= (cls.capacity / (cls.capacity * 3)) * callA
+            handB *= (cls.capacity / (cls.capacity * 3)) * handA
+            # Load at each slice
+            newL *= newB / cls.holding_time1
+            handL *= handB / cls.holding_time2
+
+            # Number calls that can be accepted
+
+            newNum = round((cls.new_thresh / bbuN) + 1)
+            handNum = round((cls.handoff_thresh / bbuH) + 1)
+            for i in range(10):
+                for newC in range(1, newNum, 1):
+                    for handC in range(1, handNum, 1):
+                        # Admissible states
+                        if (bbuN * (
+                                newC + handC) <= cls.capacity and bbuN * newC <= cls.new_thresh and
+                            bbuH * handC <= cls.handoff_thresh) or (bbuN * (
+                                newC + handC) <= cls.uRLLC_capacity and bbuN * newC <= cls.uRLLC_newcall_thresh and bbuH * handC < cls.uRLLC_handoff_thresh):
+                            for j in range(10):
+                                qn[j] = pow(newL[j], newC) / cls.factorial(newC)
+                                qh[j] = pow(handL[j], handC) / cls.factorial(handC)
+                                G[j] = G[j] + qn[j] * qh[j]  # Normalisation Constant
+
+                                # Condition for blocking new calls
+
+                                if (bbuN + (
+                                        bbuN * newC + bbuH * handC)) > cls.capacity or bbuN + bbuN * newC > \
+                                        cls.new_thresh and (bbuN + (
+                                        bbuN * newC + bbuH * handC)) > cls.uRLLC_capacity or bbuN + bbuN * newC > \
+                                        cls.uRLLC_newcall_thresh:
+                                    G1[j] = G1[j] + qn[j] * qh[j]
+
+                                # Condition of dropping new calls
+                                if (bbuN + bbuN * (
+                                        newC + handC) > cls.capacity or bbuH + bbuH * handC > cls.handoff_thresh) and \
+                                        (bbuN + bbuN * (
+                                                newC + handC) > cls.uRLLC_capacity or bbuH + bbuH * handC > cls.uRLLC_handoff_thresh):
+                                    G2[j] = G2[j] + qn[j] * qh[j]
+
+            # return the probabilities
+            blocking_prob = cls.probabilities(G1, G)
+            dropping_prob = cls.probabilities(G2, G)
+            return slice_name, blocking_prob, dropping_prob
+
+        elif slice_name == 'uRLLC':
+            required_bbu = UseCases()
+            bbuN = required_bbu.allocate_bbu(slice_name)[0]
+            bbuH = required_bbu.allocate_bbu(slice_name)[1]
+            cls.capacity = params[0]
+            cls.new_thresh = params[1]
+            cls.handoff_thresh = params[2]
+
+            # Available Bandwidth
+            newB *= (cls.capacity / (cls.capacity * 3)) * callA
+            handB *= (cls.capacity / (cls.capacity * 3)) * handA
+
+            # Load at the slice
+            newL *= newB / cls.holding_time1
+            handL *= handB / cls.holding_time2
+
+            # Number calls that can be accepted
+            newNum = round((cls.new_thresh / bbuN) + 1)
+            handNum = round((cls.handoff_thresh / bbuH) + 1)
+            for i in range(10):
+                for newC in range(1, newNum, 1):
+                    for handC in range(1, handNum, 1):
+                        # Admissible states
+                        if bbuN * (
+                                newC + handC) <= cls.capacity and bbuN * newC <= cls.new_thresh and \
+                                bbuH * handC <= cls.handoff_thresh:
+                            for j in range(10):
+                                qn[j] = pow(newL[j], newC) / cls.factorial(newC)
+                                qh[j] = pow(handL[j], handC) / cls.factorial(handC)
+                                G[j] = G[j] + qn[j] * qh[j]  # Normalisation Constant
+
+                                # Condition for blocking new calls
+                                if bbuN + (
+                                        bbuN * newC + bbuH * handC) > cls.capacity or bbuN + bbuN * newC > \
+                                        cls.new_thresh:
+                                    G1[j] = G1[j] + qn[j] * qh[j]
+
+                                # Condition for dropping handoff calls
+                                if bbuN + (
+                                        bbuN * newC + bbuH * handC) > cls.capacity or bbuH + bbuH * handC > \
+                                        cls.handoff_thresh:
+                                    G2[j] = G2[j] + qn[j] * qh[j]
+
+            # return the probabilities
+            blocking_prob = cls.probabilities(G1, G)
+            dropping_prob = cls.probabilities(G2, G)
+            return slice_name, blocking_prob, dropping_prob
+
+        elif slice_name == 'mMTC':
+            required_bbu = UseCases()
+            bbuN = required_bbu.allocate_bbu(slice_name)
+            bbuHE = required_bbu.allocate_bbu('eMBB')[1]
+            bbuHU = required_bbu.allocate_bbu('uRLLC')[1]
+            cls.capacity = params[0]
+            cls.new_thresh = params[1]
+            cls.uRLLC_handoff_thresh = params[0] + 10
+            cls.uRLLC_newcall_thresh = params[1] + 5
+            cls.uRLLC_capacity = params[0] + 10
+            cls.eMBB_handoff_thresh = params[0] + 10
+            cls.eMBB_newcall_thresh = params[1] + 5
+            cls.eMBB_capacity = params[0] + 10
+
+            # Available bandwidth
+            newB *= (cls.capacity / (cls.capacity * 3)) * callA
+            # Load at the slice
+            newL *= newB / cls.holding_time1
+
+            # Number calls that can be accepted
+            newNum = round((cls.new_thresh / bbuN) + 1)
+            handNumE = round((cls.eMBB_handoff_thresh / bbuHE) + 1)
+            handNumU = round((cls.uRLLC_handoff_thresh / bbuHU) + 1)
+            for i in range(10):
+                for newC in range(1, newNum, 1):
+                    for handCE in range(1, handNumE, 1):
+                        for handCU in range(1, handNumU, 1):
+                            # Admissible States
+                            if (bbuN * newC <= cls.capacity and bbuN * newC <= cls.new_thresh) or \
+                                    ((bbuN * newC + bbuHE * handCE) <= cls.eMBB_capacity and bbuN * newC <=
+                                     cls.eMBB_newcall_thresh and bbuHE * handCE <= cls.eMBB_handoff_thresh) or \
+                                    ((bbuN * newC + bbuHU * handCU) <= cls.uRLLC_capacity and
+                                     bbuN * newC <= cls.uRLLC_newcall_thresh and
+                                     bbuHU * handCU <= cls.uRLLC_handoff_thresh):
+                                for j in range(10):
+                                    qn[j] = pow(newL[j], newC) / cls.factorial(newC)
+                                    G[j] += qn[j]
+                                    # Blocking Conditions
+                                    if (bbuN * newC > cls.capacity or bbuN + bbuN * newC > cls.new_thresh) and \
+                                            ((bbuN + (bbuN * newC + bbuHE * handCE)) > cls.eMBB_capacity or
+                                             (bbuN + bbuN * newC) > cls.eMBB_newcall_thresh) and \
+                                            ((bbuN + (bbuN * newC + bbuHU * handCU)) > cls.uRLLC_capacity or
+                                             (bbuN + bbuN * newC) > cls.uRLLC_newcall_thresh):
+                                        G3[j] += qn[j]
             # return the probabilities
             blocking_prob = cls.probabilities(G3, G)
             return slice_name, blocking_prob
--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from Slices import Slices, Slice
 from UseCases import UseCases
-from Calculations import Calculations
+from Calculations import IntraCalculations, InterCalculations
 import matplotlib.pyplot as plt
 
 
@@ -13,6 +13,7 @@ class Simulator:
     mMTC_thresh = 15
     mMTC_cap = 15
     data_dict = {}
+    data2_dict = {}
 
     def __init__(self):
         print('Handover Algorithm initialised')
@@ -23,11 +24,14 @@ class Simulator:
         slices = ['eMBB', 'uRLLC', 'mMTC']
         for slice_name in slices:
             if slice_name is 'eMBB' or slice_name is 'uRLLC':
+                # create Slices object
                 initialise = Slices(slice_name, self.capacity, self.new_thresh, self.handoff_thresh).slice
 
             else:
+                # create a Slice object
                 initialise = Slice(slice_name, self.mMTC_cap, self.mMTC_thresh).slice
 
+        # return a dict with slice name as the key and an array of parameters as values
         return initialise
 
     @staticmethod
@@ -40,22 +44,53 @@ class Simulator:
     @property
     def execute(self):
         slice_type = self.initialise_slices
-        calc = Calculations()
-        for key in slice_type:
-            if key == 'eMBB' or key == 'uRLLC':
-                probabilities = calc.calculations(key, slice_type[key])
-                print('The blocking probability for {} slice is: {} \n'.format(probabilities[0], probabilities[1]))
-                print('The dropping probability for {} slice is: {} \n'.format(probabilities[0], probabilities[2]))
-                print('******************************************************************************************')
-                self.data_dict.update({key: probabilities[1]})
+
+        print('Choose the type of handover you want to simulate: ')
+        print('1. Intra Slice handover')
+        print('2. Inter Slice handover')
+        # choice = eval(input('Enter handover type to simulate: \n'))
+        choice = 1
+        while choice < 3:
+
+            if choice == 1:
+                print('Implementing Intra Slice handover\n')
+                calc = IntraCalculations()
+                for key in slice_type:
+                    if key == 'eMBB' or key == 'uRLLC':
+                        probabilities = calc.calculations(key, slice_type[key])
+                        print(f'The blocking probability for {probabilities[0]} slice is: {probabilities[1]} \n')
+                        print(f'The dropping probability for {probabilities[0]} slice is: {probabilities[2]} \n')
+                        print('******************************************************************************\n')
+                        self.data_dict.update({key: probabilities[1]})
+                    else:
+                        probabilities = calc.calculations(key, slice_type[key])
+                        print(f'The blocking probability for {probabilities[0]} slice is: {probabilities[1]} \n')
+                        self.data_dict.update({key: probabilities[1]})
+                data_set = pd.DataFrame(self.data_dict, index=np.arange(1.0, 6, 0.5))
+                print(data_set)
+                # data_set.reset_index().plot(x='index', y=['uRLLC'])
+                # plt.axis([0, 5, 0, 1])
+                # plt.show()
             else:
-                probabilities = calc.calculations(key, slice_type[key])
-                print('The blocking probability for {} slice is: {} \n'.format(probabilities[0], probabilities[1]))
-                self.data_dict.update({key: probabilities[1]})
-        data_set = pd.DataFrame(self.data_dict, index=np.arange(1.0, 6, 0.5))
-        print(data_set)
-        data_set.reset_index().plot(x='index', y=['uRLLC'])
-        # plt.axis([0, 5, 0, 1])
+                print('Implementing Inter Slice handover\n')
+                calc = InterCalculations()
+                for key in slice_type:
+                    if key == 'eMBB' or key == 'uRLLC':
+                        probabilities = calc.calculations(key, slice_type[key])
+                        print(f'The blocking probability for {probabilities[0]} slice is: {probabilities[1]} \n')
+                        print(f'The dropping probability for {probabilities[0]} slice is: {probabilities[2]} \n')
+                        print('******************************************************************************\n')
+                        self.data2_dict.update({key: probabilities[1]})
+                    else:
+                        probabilities = calc.calculations(key, slice_type[key])
+                        print(f'The blocking probability for {probabilities[0]} slice is: {probabilities[1]} \n')
+                        self.data2_dict.update({key: probabilities[1]})
+                data_set2 = pd.DataFrame(self.data2_dict, index=np.arange(1.0, 6, 0.5))
+            choice += 1
+
+        fig = plt.figure()
+        for data in [data_set.reset_index(), data_set2.reset_index()]:
+            plt.plot(data['index'], data['mMTC'])
         plt.show()
 
 
